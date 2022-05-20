043337905848fa298eb607cde646b5104bcb54c4
moonbi <bomrom97@naver.com>
2022-05-20 14:46:51 +0900

    [feat]add devenv

---
+++ devenv/script.js
@@ -0,0 +1,26 @@
+var hw = document.getElementById('hw');
+
+hw.addEventListener('click',function(){
+    alert('hello world')
+})
+
+var imgs = document.getElementsByTagName('img');
+//배열로 받아짐
+//imgs
+//imgs[0]
+//imgs[0].style.width = '50px';
+//객체화
+
+window.alert('Hello World');
+//window라는 객체를 불러와서 alert이라는 함수를 실행시킴
+
+//window : 전역객체
+//window, frame을 제어
+//document: window라는 객체의 property에 저장된 객체 -> 암시적으로 window.document = documetn
+
+//==========
+//BOM(Browser Object Model) = window 객체의 property 중 하나
+//Javascript core
+
+//전역 변수 : window라는 객체의 property를 만드는 것!
+//
